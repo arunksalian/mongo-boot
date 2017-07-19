60afc12666c97dd8b4dba32046108f8e6228caf0
Arun Salian <arun.salian@gmail.com>
2017-07-19 12:58:12 +0530

    Added boot dependencies and main class

--- src/main/java/com/slin/app/BootMain.java
+++ src/main/java/com/slin/app/BootMain.java
@@ -1,11 +1,13 @@
 package com.slin.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BootMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(BootMain.class, args);
 	}
 
 }
